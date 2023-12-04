b55bd780398673711abb686be505f6f9eace5431
ArjKre <crkreshnan916@gmail>
2023-12-05 01:31:34 +0530

    Adding existing project files to Git

---
+++ lib/src/pages/home_page.dart
@@ -0,0 +1,55 @@
+import 'package:blckdrop/src/widgets/custom_appbar_widget.dart';
+import 'package:blckdrop/utils/widget.dart';
+import 'package:blckdrop/utils/utils.dart';
+
+class HomePage extends StatefulWidget {
+  const HomePage({
+    super.key,
+  });
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  GlobalKey<ScaffoldState> globalKey = GlobalKey();
+  TextEditingController _controllerSearch = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      key: globalKey,
+      drawer: CustomDrawer(),
+      appBar: PreferredSize(
+        preferredSize: Size.fromHeight(90),
+        child: CustomAppBar(scaffoldkey: globalKey),
+      ),
+      backgroundColor: Colors.grey[200],
+      body: ListView(
+        physics: BouncingScrollPhysics(
+          decelerationRate: ScrollDecelerationRate.normal,
+        ),
+        children: [
+          Column(
+            children: [
+              SizedBox(
+                height: 10,
+              ),
+              //searchbar
+              SearchWidget(
+                controllerSearch: _controllerSearch,
+                hintText: "Search product",
+                icon: Icons.search_rounded,
+              ),
+              SizedBox(
+                height: 25,
+              ),
+              //brands
+              BrandSelector(),
+            ],
+          ),
+        ],
+      ),
+    );
+  }
+}
